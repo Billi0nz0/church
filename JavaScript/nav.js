9147f8bc4d31da19158e748c70f85e32887fa692
Hezekiah <irochezekiah@gmail.com>
2025-09-03 21:48:17 +0100

    Add files via upload

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,15 +1,15 @@
 function toggleNav() {
-            const nav = document.getElementById("myLinks");
-            const iconAnchor = document.getElementById("menu-icon");
-            const icon = iconAnchor.querySelector("i");
+    const nav = document.getElementById("myLinks");
+    const iconAnchor = document.getElementById("menu-icon");
+    const icon = iconAnchor.querySelector("i");
 
-            nav.classList.toggle("show");
+    nav.classList.toggle("show");
 
-            if (nav.classList.contains("show")) {
-                icon.classList.remove("fa-bars");
-                icon.classList.add("fa-times");
-            } else {
-                icon.classList.remove("fa-times");
-                icon.classList.add("fa-bars");
-            }
-        }
+    if (nav.classList.contains("show")) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+    } else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+    }
+}
