cea73941a642760b590e9ec4966679ce6bdc887b
Hezekiah <irochezekiah@gmail.com>
2026-06-13 20:06:10 +0100

    Delete JavaScript directory

--- JavaScript/aboutusslider.js
+++
@@ -1,8 +0,0 @@
-let index = 0;
-const slides = document.getElementById("slides");
-const total = slides.children.length;
-
-function moveSlide(step) {
-  index = (index + step + total) % total; // loop around
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}
--- JavaScript/preloader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-    const preloader = document.getElementById("preloader");
-    const content = document.getElementById("content");
-
-    preloader.style.display = "none"; // hide preloader
-    content.style.display = "block";  // show page content
-});
