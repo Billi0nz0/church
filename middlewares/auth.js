3a5156f3f0642e5ea4b9e7842de5e07079c4ceba
Hezekiah <irochezekiah@gmail.com>
2026-06-15 02:28:06 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -59,8 +59,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     // HIDE SIGNUP IF NOT SUPERADMIN
     // ==========================
     if (!isSuperAdmin) {
-        if (signupForm) signupForm.style.display = "none";
-        if (signupTab) signupTab.style.display = "none";
+        if (signupForm) signupForm.style.display = "";
+        if (signupTab) signupTab.style.display = "";
 
         const msg = document.createElement("p");
         msg.textContent = "Only superadmins can create accounts.";
